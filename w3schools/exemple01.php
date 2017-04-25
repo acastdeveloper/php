e0a8a26a8c81575913d668844ab83dd49332c900
Àlex Castelló <ac.desenvolupador@gmail.com>
2017-04-25 14:42:32 +0200

    Missatge posterior

--- w3schools/exemple01.php
+++ w3schools/exemple01.php
@@ -8,8 +8,32 @@
 </head>
 <body>
   <?php
+  /* Per escriure */
   echo "My first PHP script!";
+
+  /* Escriure una variable*/
+  $a = "Hola";
+  echo $a;
+
+
+
   ?>
+
+<hr>
+<?php
+//
+
+$x = 5;
+$y = 10;
+
+function myTest() {
+    global $x, $y;
+    $y = $x + $y;
+}
+
+myTest();
+echo $y; // outputs 15
+?>
 
 </body>
 </html>
